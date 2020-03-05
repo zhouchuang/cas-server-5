a6f5eff39cede5a6a1dc07e68d0964e581d39d07
zhouchuang <zhouchuang1217@163.com>
2020-03-05 17:27:53 +0800

    first

---
+++ src/main/java/user/zc/casservice5/CasService5Application.java
@@ -0,0 +1,13 @@
+package user.zc.casservice5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasService5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasService5Application.class, args);
+    }
+
+}
